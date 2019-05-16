6c037e67207f0cd691ea979985db9c441c8c025a
dathalongbay <dathalongbay@gmail.com>
2019-05-16 20:25:46 +0700

    1111

---
+++ day2/7.array/index7.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Các hàm xử lý mảng</h1>
+<?php
+    $students = array("tuan", "hung", "nam", "linh", "chi");
+    echo "<br>Đếm số phần tử trong mảng : " . count($students);
+?>
+</body>
+</html>
