0b118ce752c3db7e7a7626a45f5530c751de994b
dathalongbay <dathalongbay@gmail.com>
2019-05-30 19:28:46 +0700

    1

--- day7/appcrud/index.php
+++ day7/appcrud/index.php
@@ -13,6 +13,47 @@
  * Nạp kết nối CSDL
  */
 include_once "config.php";
+
+$sqlSelect = "SELECT * FROM employees";
+/**
+ * mysqli_query(tham số 1, tham số 2) là hàm thực hiện câu query mysql
+ * tham số 1 chính là biến kết nối CSDL $connection
+ * tham số 2 chính là câu query sql
+ */
+$result = mysqli_query($connection, $sqlSelect);
+
+/**
+ * In ra biến $result
+ * mysqli_result Object
+        (
+        [current_field] => 0
+        [field_count] => 4
+        [lengths] =>
+        [num_rows] => 0
+        [type] => 0
+        )
+ */
+
+/**
+ * mysqli_num_rows() truyền vào tham số của hàm mysqli_query()
+ * được sử dụng để đếm số bản ghi trả về của câu SQL
+ */
+if (mysqli_num_rows($result) > 0) {
+    /**
+     * mysqli_fetch_assoc() truyền vào biến của mysqli_query()
+     * nó dùng để lấy ra từng bản ghi trả về của câu SQL
+     * $row sẽ là 1 bản ghi trong bảng
+     * vòng lặp while sẽ chạy cho đến khi hết các bản ghi trong bảng trả về từ câu SQL
+     */
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<pre>";
+        print_r($row);
+        echo "</pre>";
+    }
+} else {
+    echo "<br> Không có bản ghi nào trong CSDL";
+}
+
 ?>
 
 <div class="container" style="margin-top: 100px">
