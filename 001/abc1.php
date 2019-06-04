238c57f74b61e9a436034b5df9d4fa8e879d12d0
dathalongbay <dathalongbay@gmail.com>
2019-06-04 19:54:43 +0700

    1111

--- 001/abc1.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: T3H
- * Date: 5/9/2019
- * Time: 6:51 PM
- */
-echo "abc";
-echo "abc";
---
+++ day1/index5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>In ra chuỗi : chào 'hà nội'</h1>
+<h1>In ra chuỗi : chào "hà nội"</h1>
+
+<?php
+echo "<br> 'chào hà nội'";
+echo "<br> \"chào hà nội\"";
+?>
+
+</body>
+</html>
