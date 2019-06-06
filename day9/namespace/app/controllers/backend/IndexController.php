894535d407f9fc0840ce33732ece6d4208397aad
dathalongbay <dathalongbay@gmail.com>
2019-06-06 20:51:38 +0700

    1111

--- day9/namespace/app/controllers/backend/IndexController.php
+++ day9/namespace/app/controllers/backend/IndexController.php
@@ -1,10 +1,26 @@
 <?php
 namespace App\Controllers\Backend;
 
+use App\Controllers\Frontend;
+use App\Models;
+
 class IndexController {
 
 
     public function indexAction() {
         echo "<br>" . __METHOD__;
+
+        $this->useOtherNamespace();
+    }
+
+    public function useOtherNamespace() {
+
+        $controlerFe = new Frontend\IndexController();
+
+        $controlerFe->indexAction();
+
+        $model = new Models\CommonModel();
+        $model->indexAction();
+
     }
 }
