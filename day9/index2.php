393129d43ce5c6e3be24d824ce7e1c97e5b14c9a
dathalongbay <dathalongbay@gmail.com>
2019-06-06 19:06:40 +0700

    1111

---
+++ day9/index2.php
@@ -0,0 +1,22 @@
+<?php
+class Connguoi {
+
+    public $name;
+
+    public function getInfo() {
+        echo "<br>" . __METHOD__;
+    }
+
+}
+
+/**
+ * Chỉ có thể kế thừa từ 1 class
+ * Class Nhanvien
+ */
+class Nhanvien extends Connguoi {
+
+}
+
+/**
+ * Đa kế thừa dùng trait
+ */
